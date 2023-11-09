70d8d58b8f9fac6cbaf18cce22039a8a13b02e37
HermanSkop <armaton111@gmail.com>
2023-11-09 18:04:56 +0100

    merge conflicts resolved

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(next);
-    next();
+    res.sendFile('main.html', { root: 'public' });
 });
 
 router.get('/', function(req, res, next) {
