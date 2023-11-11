5a8efd87be7a27e7fa884aa92819f4c85d3b8ca0
HermanSkop <armaton111@gmail.com>
2023-11-11 11:24:00 +0100

    Message after deadline of timer is updated

--- public/js/timer.js
+++ public/js/timer.js
@@ -18,7 +18,7 @@
 
             //do something later when date is reached
             if (distance < 0) {
-                document.getElementById("headline").innerText = "It's my birthday!";
+                document.getElementById("headline").innerText = "Время скидки вышло!";
                 document.getElementById("countdown").style.display = "none";
                 document.getElementById("content").style.display = "block";
                 clearInterval(x);
